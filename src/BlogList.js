2eb8b66160e49e78a057a0b516ebfe83d8f31f73
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-05 15:31:18 +0900

    Added a custom hook changed the states of the home component to stateless

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 const BlogList = ({blogs, title}) => {
  //You can destructure the props directly on the function
-  //const blogs = props.blogs;
+  //const blogs = props.blogs; <-- we dont need these anymore because the props are automatically decoconstructed
   // const title = props.title;
 
   // functions that change the state can be passed on as props BUT the function itself should be placed in the component that contains the state so that it can change the state direcrtly
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,26 @@
-import { useState, useEffect } from "react";
+
 import BlogList from "./BlogList";
+import useFetch from "./useFetch"; // import custom hook
 
 const Home = () => {
-  //Deconstructing array variable "name" and a function which we can name whatever 
+  //Deconstructing array variable and a function which we can name whatever usually with a prefix "set"
   // useState is called a HOOK. So it can hook to an event listener and changes the state of the component
   
-  //States 
-  const [blogs, setBlogs] = useState(null);
+  //States of the Home component - must import useState first
+  /*const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+  */
+
+  // useFetch() is a Custom hook: it is used so that it can be used in any component so states and useEffect hooks are reused whenever an API fetching is needed
+
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
   // Props are data passed on from the parent component to the children components
   // components are be reusable with the same props but data can be manipulated through array methods
 
   //useEffect is another React hook (e.g. useState) that fires off whenever there is a change in render. This is useful when you have to fetch data whenever the component state is changed
-    useEffect(() => {
+  /*  useEffect(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
           if(!res.ok) {
@@ -32,7 +38,8 @@ const Home = () => {
           setError(err.message);
           console.log(err.message);
         })
-    }, []); //<-- This dependency array is important because this array tells useEffect hook to only run for a certain render changes and NOT every time.
+    }, []);
+    */ //<-- This dependency array is important because this array tells useEffect hook to only run for a certain render changes and NOT every time.
 // the name variable and its state is now a dependency in its array, so useEffect will only run IF the name state is changed
 // Logical templating in reac { variable && <html> } is used for aynschronous API. It means if the variable is true then the right side of && is fired off. 
     // If we dont use the { variable && <html> } it will give an error because it takes some time to get the data and the html is already firing off.
